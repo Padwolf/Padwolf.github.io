c7e0ba325e34a42b3e231a2ca956e610875ca0aa
Patrick  Duane <970098955@aps.edu>
2016-09-20 18:49:55 -0600

    Tue 09/20/2016 18:49:55.00: Another updater test - 1

--- ftc-2016-2017/ftcws-update.bat
+++ ftc-2016-2017/ftcws-update.bat
@@ -6,7 +6,20 @@ rmdir /S /Q ftc-2016-2017
 xcopy /s C:\Users\970098955\Programming\FTC\website C:\Users\970098955\Programming\HTML\PersonalSite\ftc-2016-2017
 git add *
 git commit -m "%DATE% %TIME%: %commitMessage%"
-echo Done
+echo Committed
+:push
+set /p push="Do you want to push the changes to the GitHub? [Y]es/[N]o: "
+if /I "%push%"=="y" (
+	git push -u origin master
+	echo Pushed
+	goto:eof
+)
+if /I "%push%"=="n" (
+	echo Done
+	goto:eof
+)
+echo "Please say [Y]es or [N]o"
+goto:push
 pause
 goto:eof
 :cancel
