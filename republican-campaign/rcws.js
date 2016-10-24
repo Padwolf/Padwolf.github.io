be56a8496913cb790df36398e569ac4568917272
Patrick  Duane <970098955@aps.edu>
2016-10-24 11:40:11 -0600

    11:40:11.52: Design

---
+++ republican-campaign/rcws.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+function setWidthP(element, percent){
+	element.width=window.innerWidth * percent;
+}
+
+function setHeightP(element, percent){
+	element.height=window.innerHeight * percent;
+}
